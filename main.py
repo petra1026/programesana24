51e514541e236e4ed18dfcaf4dfcc860a54db207
petra1026 <149487838+petra1026@users.noreply.github.com>
2024-03-20 14:20:39 +0200

    done

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app= Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/teksts")
+def teksts():
+    return render_template("teksts.html")
+
 if __name__ == '__main__':
     app.run(port = 5000)
     
