97089de7c4a189712fcaf27e43a9108e7b387a74
petra1026 <149487838+petra1026@users.noreply.github.com>
2024-03-20 13:06:29 +0200

    ff

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
-app=Flask(__name__)
+app= Flask(__name__)
+
 @app.route("/")
 def index():
     return"Sveiki!"
+
 if __name__ == '__main__':
     app.run(port = 5000)
     
